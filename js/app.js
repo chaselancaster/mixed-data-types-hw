badc39da832ce15ea8d187b9d0aa0a67228edddc
chaselancaster <chaseblancaster@gmail.com>
2019-03-25 16:00:23 -0700

    data types are complete

---
+++ js/app.js
@@ -0,0 +1,47 @@
+// 1. Data Types
+
+// For each of the following, write which data types you would use to 
+// represent the data, and then give a small example of the data structure:
+
+//A light switch that can be either on or off.
+// Boolean.  Light switch on would either be TRUE or FALSE
+// lightSwitchOn === true;
+
+// A user's email address.
+// This would be a sting probably stored in a user object.
+// const user = {
+//     email: 'test@test.com'
+// }
+
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// This would be an array
+// const spaceship = ['hull', 'laser blasters', 'tractor beam', 'warp drive']
+
+// A list of student names from our class
+// This would be an array.
+// const studentNames = ['name1', 'name2', 'name3'];
+
+// A list of student names from our class, each with a location.
+// This would be objects inside an object
+// const studentNames = {
+//     student1 = {
+//         location: 'LA'
+//     }
+//     student2 = {
+//         location: 'SF'
+//     }
+// }
+
+// A list of student names from our class, each with a location and 
+// each with a list of favorite tv shows.
+// Same as above but with added keys.  The TV shows would be an array.
+// const studentNames = {
+//     student1 = {
+//         location: 'LA',
+//         tvshows: []
+//     }
+//     student2 = {
+//         location: 'SF'
+//         tvshows: []
+//     }
+// }
